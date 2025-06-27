ecd8283b52cfcd33c791af240416438f6f37b955
gpkuster <gpastor.kuster@gmail.com>
2025-06-27 10:31:00 +0200

    Include ReentrancyGuard at SmartVault.sol

--- contracts/SmartVault.sol
+++ contracts/SmartVault.sol
@@ -2,7 +2,9 @@
 
 pragma solidity ^0.8.0;
 
-contract SmartVault {
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract SmartVault is ReentrancyGuard {
     address public owner;
     uint public maxBalance;
 
@@ -12,11 +14,13 @@ contract SmartVault {
     }
 
     receive() external payable {
+        // address(this).balance is the balance including the amount trying to receive at the moment
         require(address(this).balance <= maxBalance, "Max balance exceeded");
     }
 
-    function withdraw(uint amount) external {
+    function withdraw(uint amount) external nonReentrant {
         require(msg.sender == owner, "Not owner");
+        require(address(this).balance >= amount, "Not enough balance");
         payable(owner).transfer(amount);
     }
 }
