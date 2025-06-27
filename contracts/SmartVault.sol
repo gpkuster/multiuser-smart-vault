6ebed460244fa27be8ba44c7a2de8b5079aad159
gpkuster <gpastor.kuster@gmail.com>
2025-06-27 11:50:42 +0200

    Add events and modfiers to SmartVault

--- contracts/SmartVault.sol
+++ contracts/SmartVault.sol
@@ -5,8 +5,24 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract SmartVault is ReentrancyGuard {
-    address public owner;
-    uint public maxBalance;
+    address public immutable owner;
+    uint256 public immutable maxBalance;
+
+    bool public paused;
+
+    event Deposit(address indexed sender, uint256 amount);
+    event Withdraw(address indexed recipient, uint256 amount);
+    event Paused(bool status);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    modifier notPaused() {
+        require(!paused, "Vault is paused");
+        _;
+    }
 
     constructor(address _owner, uint _maxBalance) {
         owner = _owner;
@@ -16,11 +32,18 @@ contract SmartVault is ReentrancyGuard {
     receive() external payable {
         // address(this).balance is the balance including the amount trying to receive at the moment
         require(address(this).balance <= maxBalance, "Max balance exceeded");
+        require(!paused, "Vault is paused");
+        emit Deposit(msg.sender, msg.value);
     }
 
-    function withdraw(uint amount) external nonReentrant {
-        require(msg.sender == owner, "Not owner");
+    function withdraw(uint amount) external onlyOwner notPaused nonReentrant {
         require(address(this).balance >= amount, "Not enough balance");
         payable(owner).transfer(amount);
+        emit Withdraw(owner, amount);
+    }
+
+    function setPaused(bool _paused) external onlyOwner {
+        paused = _paused;
+        emit Paused(_paused);
     }
 }
