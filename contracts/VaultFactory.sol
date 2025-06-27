1894339e12fbb2cdc7d29d87deca24757c087b1c
gpkuster <gpastor.kuster@gmail.com>
2025-06-27 10:18:52 +0200

    Add event to VaultFactory.sol

--- contracts/VaultFactory.sol
+++ contracts/VaultFactory.sol
@@ -5,10 +5,14 @@ pragma solidity ^0.8.0;
 import "./SmartVault.sol";
 
 contract VaultFactory {
+    // allows one user to have multiple vaults
     mapping(address => address[]) public userVaults;
+
+    event VaultCreated(address owner, address vaultAddress, uint256 maxBalance);
 
     function createVault(uint _maxBalance) external {
         SmartVault newVault = new SmartVault(msg.sender, _maxBalance);
         userVaults[msg.sender].push(address(newVault));
+        emit VaultCreated(msg.sender, address(newVault), _maxBalance);
     }
 }
