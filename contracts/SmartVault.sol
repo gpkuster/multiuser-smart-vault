8a65f32b6191a6e6f150b4363f783fd89e5854c3
gpkuster <gpastor.kuster@gmail.com>
2025-06-30 21:32:12 +0200

    Add getVaultBalance, getVaultCount  and getBalance. Use NatSpec

--- contracts/SmartVault.sol
+++ contracts/SmartVault.sol
@@ -4,6 +4,11 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
+
+/**
+ * @title SmartVault
+ * @notice Individual Smart Vault, managed by one single owner
+ */
 contract SmartVault is ReentrancyGuard {
     address public immutable owner;
     uint256 public immutable maxBalance;
@@ -29,6 +34,7 @@ contract SmartVault is ReentrancyGuard {
         maxBalance = _maxBalance;
     }
 
+    /// @notice Recieve Ether and verify max balance
     receive() external payable {
         // address(this).balance is the balance including the amount trying to receive at the moment
         require(address(this).balance <= maxBalance, "Max balance exceeded");
@@ -36,14 +42,21 @@ contract SmartVault is ReentrancyGuard {
         emit Deposit(msg.sender, msg.value);
     }
 
+    /// @notice Allows owner to withdraw Ether
     function withdraw(uint amount) external onlyOwner notPaused nonReentrant {
         require(address(this).balance >= amount, "Not enough balance");
         payable(owner).transfer(amount);
         emit Withdraw(owner, amount);
     }
 
+    /// @notice Activates or deactivates emergency mode
     function setPaused(bool _paused) external onlyOwner {
         paused = _paused;
         emit Paused(_paused);
+    }
+
+    /// @notice Allows checking a vault's balance
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
     }
 }
--- contracts/VaultFactory.sol
+++ contracts/VaultFactory.sol
@@ -4,15 +4,32 @@ pragma solidity ^0.8.0;
 
 import "./SmartVault.sol";
 
+/**
+ * @title VaultFactory
+ * @notice Multiuser Smart Vault Factory
+ */
 contract VaultFactory {
-    // allows one user to have multiple vaults
+    // Allows one user to have multiple vaults
     mapping(address => address[]) public userVaults;
 
     event VaultCreated(address owner, address vaultAddress, uint256 maxBalance);
 
+    /// @notice Creates a new SmartVault for the user
     function createVault(uint _maxBalance) external {
         SmartVault newVault = new SmartVault(msg.sender, _maxBalance);
         userVaults[msg.sender].push(address(newVault));
         emit VaultCreated(msg.sender, address(newVault), _maxBalance);
+    }
+
+    /// @notice Returns the balance of one user by its index
+    function getVaultBalance(address user, uint256 index) external view returns (uint256) {
+        require(index < getVaultCount(user), "Invalid index");
+        SmartVault vault = SmartVault(payable(userVaults[user][index]));
+        return vault.getBalance();
+    }
+
+    /// @notice Returns the amount of vaults that a user owns
+    function getVaultCount(address user) public view returns (uint256) {
+        return userVaults[user].length;
     }
 }
